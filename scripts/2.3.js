2faa6ff43c108f10f2dd1f8eeea4f3fe5b4eb971
Justin Michel <justinmichel2000@gmail.com>
2023-09-08 14:56:03 +0200

    2.3

---
+++ scripts/2.3.js
@@ -0,0 +1,21 @@
+
+
+for (let i = 0; i <= 100; i += 2)  {
+
+   console.log(i);
+} 
+
+let i = 0
+
+while (i <= 100) {
+
+    // console.log(i);
+    i += 1;
+
+    if (i % 2 == 0) {
+
+     console.log(i);
+    }
+
+}
+
