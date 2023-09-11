72256fb7e7cf20c2e1e7c4c3c892d74d32fc9bb8
Justin Michel <justinmichel2000@gmail.com>
2023-09-11 10:32:51 +0200

    3.1

---
+++ scripts/3.1.js
@@ -0,0 +1,22 @@
+
+// let numbersArray = [1, 2, 3, 4, 5];
+
+// let sum = 0;
+
+// for (let elem of numbersArray) {
+
+//     sum += elem;
+// }
+
+// console.log("The sum of the elements of the array is " + sum);
+
+let numbersArray = [100, 101, 102];
+
+let sum = 0;
+
+for (let elem of numbersArray) {
+
+    sum += elem;
+}
+
+console.log("The sum of the elements of the array is " + sum);
