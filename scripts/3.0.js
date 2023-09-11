bf157a1586291a1368042e11b8e9039fed33ec25
Justin Michel <justinmichel2000@gmail.com>
2023-09-11 10:19:53 +0200

    3.0

---
+++ scripts/3.0.js
@@ -0,0 +1,8 @@
+let arr = ["Alexandre", "Antoine", "Bastien", "Carole", "Dorian" , "Elisabeth", 
+"Elodie", "Justin" , "Justine", "Justine", "Kimi", "Layla", "Lidwine", "Lucas", 
+"Marie", "Mathias", "Okly", "Pierre", "Robin", 
+"Rosalie", "Stephane", "Tim", "Tom", "Valentin", "Alexandre", "Virginie"];
+
+for (let elem of arr) {
+  console.log(elem);
+}
