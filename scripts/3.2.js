f21ccf8dd1f6291a2ed9bc1996de0e6b8fb46bf1
Justin Michel <justinmichel2000@gmail.com>
2023-09-11 10:53:16 +0200

    3.2

---
+++ scripts/3.2.js
@@ -0,0 +1,36 @@
+
+// let numbersArray = [1, 2, 3, 4, 5];
+
+// let average = 0;
+
+// let sum = 0;
+
+// for (let elem of numbersArray) {
+
+//     sum += elem;
+// }
+
+// if (numbersArray.length > 0) {
+
+//     average = sum / numbersArray.length;
+// }
+
+// console.log("The average value of this array is " + average);
+
+let numbersArray = [100, 101, 102];
+
+let average = 0;
+
+let sum = 0;
+
+for (let elem of numbersArray) {
+
+    sum += elem;
+}
+
+if (numbersArray.length > 0) {
+
+    average = sum / numbersArray.length;
+}
+
+console.log("The average value of this array is " + average);
---
+++ scripts/3.3.js
@@ -0,0 +1,6 @@
+// Write a program that will create a duplicate of a given array.
+
+// Bonus: make a first version that will only do it using push(). 
+// Make a second version that uses a single method call to perform 
+// the copy. 
+// (You'll have to search on MDN or Google for this one.)
